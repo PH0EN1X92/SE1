89610ab97a79a5b91647d4ad4dc0c1475873848b
Ilyushin Kirill <netrunner093@gmail.com>
2023-12-26 23:02:46 +0500

    added x and y

--- tp.cpp
+++ tp.cpp
@@ -3,12 +3,5 @@ using namespace std;
 
 int main()
 {
-    int x, y;
-    cout << "vvedite chisa: \n" << "x = "; 
-    cin >> x;
-    cout << "y = "; 
-    cin >> y;
-    cout << x << "+" << y << "=" << x+y << endl;
 
-    return 0; 
 }
