4cd0c6c285880e05f6952f260881e6adf1801498
Ilyushin Kirill <netrunner093@gmail.com>
2023-12-26 23:49:13 +0500

    added plus and minus

--- tp.cpp
+++ tp.cpp
@@ -4,10 +4,12 @@ using namespace std;
 int main()
 {
     int x, y;
-    cout << "vvedite chisa: \n" << "x = "; 
+    cout << "vvedite chisla: \n" << "x = "; 
     cin >> x;
     cout << "y = "; 
     cin >> y;
-  
+    
+    cout << x << "+" << y << "=" << x+y << endl;
+    cout << x << "-" << y << "=" << x-y << endl;
     return 0; 
 }
