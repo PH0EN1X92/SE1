3395f0bf58f2f3d423f60c646dfecab1126a6dfd
Ilyushin Kirill <netrunner093@gmail.com>
2023-12-26 21:24:01 +0500

    new file:   .vscode/c_cpp_properties.json 	new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	new file:   tp.cpp

---
+++ tp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cout << "vvedite chisa: \n" << "x = "; 
+    cin >> x;
+    cout << "y = "; 
+    cin >> y;
+    return 0; 
+}
